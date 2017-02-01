b93cc9edef8701b0ab354bb1ce32f2dc1b372d61
rtn-martavis <martavis@roadtripnation.com>
2017-02-01 13:41:53 -0800

    Initial commit of project structure

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <div className="header">
+                    <h1>NCSoft Test</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
